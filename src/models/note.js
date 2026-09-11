ece414f0a2015322d09276827e2d5acdbc18160f
Liliia Fodorova <nevskay22@gmail.com>
2026-09-12 00:45:52 +0200

    add mongodb integration and notesroutes

---
+++ src/models/note.js
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+  title:{
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    default: '',
+    trim: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+    enum: [
+      'Work',
+      'Personal',
+      'Meeting',
+      'Shopping',
+      'Ideas',
+      'Travel',
+      'Finance',
+      'Health',
+      'Important',
+      'Todo',
+    ],
+    default: 'Todo',
+  },
+},
+{
+  timestamps: true,
+  versionKey: false,
+}
+);
+
+export const Note = model('note', noteSchema);
+// export default Note;
